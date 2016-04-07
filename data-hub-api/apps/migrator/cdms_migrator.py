e1b422b3598621c85c964cfeeacb0d9cf92d5a69
Marco Fucci <marcofucci@gmail.com>
2016-04-07 17:33:23 +0100

    Removed unused function

--- data-hub-api/apps/migrator/cdms_migrator.py
+++ data-hub-api/apps/migrator/cdms_migrator.py
@@ -87,25 +87,3 @@ class BaseCDMSMigrator(object):
             setattr(local_obj, field_name, value)
 
         return local_obj
-
-    def get_conflicting_fields(self, local_obj, cdms_data):
-        """
-        Returns the list of fields conflicting between local_obj and cdms_data.
-        """
-        conflicting_fields = {}
-        for field in local_obj._meta.fields:
-            field_name = field.name
-            try:
-                cdms_field = self.get_cdms_field(field_name)
-            except NotMappingFieldException:
-                continue
-
-            cdms_value = cdms_field.from_cdms_value(cdms_data[cdms_field.cdms_name])
-            local_value = getattr(local_obj, field_name)
-
-            if cdms_value != local_value:
-                conflicting_fields[field_name] = {
-                    'theirs': cdms_value,
-                    'yours': local_value
-                }
-        return conflicting_fields
