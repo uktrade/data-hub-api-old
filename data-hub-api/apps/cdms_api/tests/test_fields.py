7dea30a47a3fd23cec8fe0b1fa89b4ac5c49d56f
Marco Fucci <marcofucci@gmail.com>
2016-04-08 11:42:21 +0100

    Add tests for cdms_api.fields

--- data-hub-api/apps/cdms_api/tests/test_fields.py
+++ data-hub-api/apps/cdms_api/tests/test_fields.py
@@ -1,8 +1,9 @@
 import datetime
+from unittest import mock
 
 from django.test.testcases import TestCase
 
-from cdms_api.fields import StringField, IntegerField, DateTimeField, BooleanField
+from cdms_api.fields import StringField, IntegerField, DateTimeField, BooleanField, IdRefField, ForeignKeyField
 
 
 class StringFieldTestCase(TestCase):
@@ -132,6 +133,75 @@ class DateTimeFieldTestCase(TestCase):
         self.assertEqual(
             self.field.to_cdms_value(dt),
             '/Date(1451606400000)/'
+        )
+
+    def test_to_cdms_value_None(self):
+        self.assertEqual(
+            self.field.to_cdms_value(None),
+            None
+        )
+
+
+class IdRefFieldTestCase(TestCase):
+    def setUp(self):
+        super(IdRefFieldTestCase, self).setUp()
+        self.field = IdRefField('name')
+
+    def test_from_cdms_value(self):
+        self.assertEqual(
+            self.field.from_cdms_value({'Id': 100}),
+            100
+        )
+
+    def test_from_cdms_value_None(self):
+        self.assertEqual(
+            self.field.from_cdms_value(None),
+            None
+        )
+
+    def test_to_cdms_value(self):
+        self.assertEqual(
+            self.field.to_cdms_value(100),
+            {'Id': 100}
+        )
+
+    def test_to_cdms_value_None(self):
+        self.assertEqual(
+            self.field.to_cdms_value(None),
+            None
+        )
+
+
+class MyFKModel(object):
+    def __init__(self, cdms_pk=None):
+        self.cdms_pk = cdms_pk
+
+
+class ForeignKeyFieldTestCase(TestCase):
+    def setUp(self):
+        super(ForeignKeyFieldTestCase, self).setUp()
+        self.field = ForeignKeyField('name', 'MyModel')
+
+    @mock.patch('cdms_api.fields.apps')
+    def __call__(self, result, mocked_apps, *args, **kwargs):
+        mocked_apps.get_model.return_value = MyFKModel
+        super(ForeignKeyFieldTestCase, self).__call__(result, *args, **kwargs)
+
+    def test_from_cdms_value(self):
+        model = self.field.from_cdms_value({'Id': 100})
+        self.assertEqual(model.cdms_pk, 100)
+
+    def test_from_cdms_value_None(self):
+        self.assertEqual(
+            self.field.from_cdms_value(None),
+            None
+        )
+
+    def test_to_cdms_value(self):
+        model = MyFKModel(cdms_pk=100)
+        self.assertEqual(
+            self.field.to_cdms_value(model),
+            {'Id': 100}
         )
 
     def test_to_cdms_value_None(self):
