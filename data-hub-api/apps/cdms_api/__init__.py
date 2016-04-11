960f77cce5f08ec3be63e7a3c6dcd46248eb6950
Marco Fucci <marcofucci@gmail.com>
2016-04-11 17:21:18 +0100

    Move cdms api to different module
    That's because it's annoying when the lib automatically logs you into
    cdms when you only want to import a different module (e.g. utils)

--- data-hub-api/apps/cdms_api/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-from .base import CDMSApi
-
-if settings.IN_TESTING:
-    from .tests.utils import get_mocked_api
-    api = get_mocked_api()
-else:
-    api = CDMSApi()
