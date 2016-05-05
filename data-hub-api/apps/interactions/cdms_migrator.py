9c07fb0de858c2ff59db3ecad05eab8d2fde2c1c
Daniel Quinn <code@danielquinn.org>
2016-05-05 17:47:13 +0100

    This should *theoreticaly* work

--- data-hub-api/apps/interactions/cdms_migrator.py
+++ data-hub-api/apps/interactions/cdms_migrator.py
@@ -4,24 +4,23 @@ from migrator.cdms_migrator import BaseCDMSMigrator
 
 
 class InteractionMigrator(BaseCDMSMigrator):
-    fields = {
-        'first_name': cdms_fields.StringField('FirstName'),
-        'last_name': cdms_fields.StringField('LastName'),
-        'organisation': cdms_fields.ForeignKeyField(
-            'ParentCustomerId',
-            fk_model='organisation.Organisation'
-        )
-    }
-    service = 'Contact'
+    service = 'ActivityPointer'
 
 
 class EmailInteractionMigrator(BaseCDMSMigrator):
     fields = {
-        'first_name': cdms_fields.StringField('FirstName'),
-        'last_name': cdms_fields.StringField('LastName'),
+        'sender_address': cdms_fields.StringField('Sender'),
+        'receiver_address': cdms_fields.StringField('ToRecipients'),
+        'message_id': cdms_fields.StringField('MessageId'),
+        'subject': cdms_fields.StringField('Subject'),
         'organisation': cdms_fields.ForeignKeyField(
             'ParentCustomerId',
             fk_model='organisation.Organisation'
         )
     }
-    service = 'Contact'
+    service = 'Email'
+
+    def update_cdms_data_from_values(self, values, cdms_data):
+        r = super(EmailInteractionMigrator, self).update_cdms_data_from_values(values, cdms_data)
+        r["ActivityTypeCode"] = "Email"
+        return r
--- data-hub-api/apps/interactions/models.py
+++ data-hub-api/apps/interactions/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from reversion import revisions as reversion
 
+from core.fields import UKTIForeignKey
 from core.models import CRMBaseModel
 from core.managers import CRMManager
 from .cdms_migrator import InteractionMigrator, EmailInteractionMigrator
@@ -20,13 +21,24 @@ class Interaction(CRMBaseModel):
 
 
 @reversion.register()
-class EmailInteraction(Interaction):
+class EmailInteraction(CRMBaseModel):
+
+    # Strictly speaking, this shouldn't be a foreign key so much as a
+    # polymorphic primary key, but merging django-polymorphic with our own
+    # CDMS-dependent architecture is asking for a headache.
+    interaction = UKTIForeignKey(Interaction, related_name="emails")
+
+    message_id = models.CharField(max_length=64)
 
     sender = models.ForeignKey(Contact)
+    sender_address = models.EmailField()  # Can be removed post-CDMS
+
     receiver = models.ForeignKey(Contact)
+    receiver_address = models.EmailField()  # Can be removed post-CDMS
+
+    subject = models.CharField(max_length=256)
     body = models.TextField()
     raw = models.TextField()
-    created = models.DateTimeField(editable=False)
 
     objects = CRMManager()
     cdms_migrator = EmailInteractionMigrator()
