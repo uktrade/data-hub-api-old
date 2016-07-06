5c999ccb36fa57e6863e535278f77c9077b6527b
James Cooke <jamescooke@users.noreply.github.com>
2016-07-06 11:52:28 +0100

    Remove testing.com and replace with example.com
    example.com is a reserved domain, whereas testing.com exists.

--- data-hub-api/settings/testing.py
+++ data-hub-api/settings/testing.py
@@ -5,9 +5,9 @@ INSTALLED_APPS += (
     'migrator.tests',
 )
 
-CDMS_ADFS_URL = 'https://adfs.testing.com'
-CDMS_BASE_URL = 'https://testing.com'
-CDMS_RSTS_URL = 'https://sso.testing.com'
+CDMS_ADFS_URL = 'https://adfs.example.com'
+CDMS_BASE_URL = 'https://example.com'
+CDMS_RSTS_URL = 'https://sso.example.com'
 CDMS_USERNAME = 'username'
 CDMS_PASSWORD = 'password'
 CDMS_COOKIE_KEY = b'RKfgWE-GYNy3mWHm5wEUZavralDzSMKDguBfyuBag8A='
