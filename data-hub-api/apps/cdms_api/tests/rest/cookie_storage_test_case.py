9693f9ac9dec6f1e0fbcf127662eab6f7d4f649b
James Cooke <jamescooke@users.noreply.github.com>
2016-08-05 16:30:45 +0100

    Extract COOKIE_FILE to settings and test CookieStorageTestCase
    `COOKIE_FILE` was module-level code which is hard to patch - it looks
    more like a setting to me, so move it to settings.
    
    Test that tearDown of CookieStorageTestCase is stable - it had been
    failing often, especially after failed test runs.

--- data-hub-api/apps/cdms_api/tests/rest/cookie_storage_test_case.py
+++ data-hub-api/apps/cdms_api/tests/rest/cookie_storage_test_case.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from cdms_api.cookie_storage import CookieStorage
 
@@ -11,12 +11,63 @@ class CookieStorageTestCase(TestCase):
         """
         super().setUp()
         self.cookie_storage = CookieStorage()
-        self.assertFalse(self.cookie_storage.exists())
 
     def tearDown(self):
         """
         Delete any cookies created during tests
         """
         self.cookie_storage.reset()
-        self.assertFalse(self.cookie_storage.exists())
         super().tearDown()
+
+
+class TestCookieStorageTestCaseSetUp(CookieStorageTestCase):
+
+    def test_clean(self):
+        """
+        CookieStorageTestCase starts up with no cookie file
+        """
+        self.assertFalse(self.cookie_storage.exists())
+
+
+@override_settings(COOKIE_FILE='/tmp/_test_cookie.tmp')
+class TestCookieStorageTestCaseTearDown(TestCase):
+    """
+    Ideally this would make use of pyfakefs or similar which would guarantee no
+    naming clashes and teardown the file system automatically at the end of
+    testing. For now continue with filesystem.
+    """
+
+    def setUp(self):
+        """
+        Prepare CookieStorageTestCase for testing, create cookie storage file
+        """
+        super().setUp()
+        CookieStorageTestCase.fake_test = lambda _: None
+        self.test_case = CookieStorageTestCase('fake_test')
+        self.test_case.setUp()
+        CookieStorage().write('stuff')
+
+    def tearDown(self):
+        del CookieStorageTestCase.fake_test
+        CookieStorage().reset()
+        super().tearDown()
+
+    def test_set_up(self):
+        """
+        CookieStorage file is available for testing tearDown
+        """
+        self.assertTrue(CookieStorage().exists())
+
+    def test_clean(self):
+        """
+        CookieStorageTestCase tearDown cleans up cookie storage file
+        """
+        self.test_case.tearDown()
+
+        self.assertFalse(CookieStorage().exists())
+
+    def test_clean_2(self):
+        """
+        CookieStorageTestCase tearDown passes when there is no cookie file
+        """
+        self.test_case.tearDown()
