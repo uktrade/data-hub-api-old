0621ca8425e26ba79fad56e920da72ae3036ba29
James Cooke <jamescooke@users.noreply.github.com>
2016-08-15 12:34:16 +0100

    Allow NTLM related settings to be overridden by envvars

---
+++ data-hub-api/apps/cdms_api/tests/test_env.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.conf import settings
+
+
+class TestEnv(TestCase):
+
+    def test_happy(self):
+        """
+        Default values for settings are loaded
+
+        This serves to test the default fall through for these settings since
+        they can be overridden by the environment.
+        """
+        self.assertEqual(settings.CDMS_BASE_URL, 'https://example.com')
+        self.assertEqual(settings.CDMS_USERNAME, 'username')
+        self.assertEqual(settings.CDMS_PASSWORD, 'password')
