5e5c55d5d6ac9b5008a8750aeee70f2e5d1f46a8
James Cooke <jamescooke@users.noreply.github.com>
2016-08-18 15:24:53 +0100

    Test that environment variables are converted into Django settings
    Test works in two modes "CircleCI mode" and "Jenkins mode".
    
    * In "CircleCI mode" there are no integration tests run and not env vars
      matching these settings are set. The default values will be used for
      all values.
    
    * In "Jenkins mode" the integration tests will be run with custom
      configuration and custom database, etc. These tests will ensure that
      the environment variables have had their values loaded into the
      appropriate Django settings.

--- data-hub-api/apps/cdms_api/tests/test_env.py
+++ data-hub-api/apps/cdms_api/tests/test_env.py
@@ -1,18 +1,149 @@
+import os
+
 from django.conf import settings
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 
-class TestEnv(TestCase):
+class EnvSettingsAssertion:
 
-    def test_happy(self):
+    def assertSettingEnvExpected(self, environment_name, django_name, default):
         """
-        Default values for settings are loaded
-
-        This serves to test the default fall through for these settings since
-        they can be overridden by the environment.
+        Given a pair of environment variable and Django setting names and a
+        default value, assert that Django loaded the expected value into the
+        Django setting.
         """
-        self.assertEqual(settings.CDMS_BASE_URL, 'https://example.com')
-        self.assertEqual(settings.CDMS_USERNAME, 'username')
-        self.assertEqual(settings.CDMS_PASSWORD, 'password')
-        with self.assertRaises(AttributeError):
-            settings.TEST_INTEGRATION
+        django_value = getattr(settings, django_name)
+
+        try:
+            expected_value = os.environ[environment_name]
+        except KeyError:
+            expected_value = default
+
+        self.assertEqual(django_value, expected_value)
+
+
+class TestEnv(TestCase, EnvSettingsAssertion):
+    """
+    Asserts that default values for settings are loaded if no envvar is set. In
+    the case that the suite is run when there *is* an envvar, then the expected
+    value is checked.
+
+    This serves to test the default fall through for these settings since they
+    can be overridden by the environment.
+    """
+
+    def test_CDMS_USERNAME(self):
+        """
+        CDMS_USERNAME setting is loaded as expected
+        """
+        self.assertSettingEnvExpected(
+            environment_name='DJANGO__CDMS_USERNAME',
+            django_name='CDMS_USERNAME',
+            default='username',
+        )
+
+    def test_CDMS_PASSWORD(self):
+        """
+        CDMS_PASSWORD setting is loaded as expected
+        """
+        self.assertSettingEnvExpected(
+            environment_name='DJANGO__CDMS_PASSWORD',
+            django_name='CDMS_PASSWORD',
+            default='password',
+        )
+
+    def test_CDMS_BASE_URL(self):
+        """
+        CDMS_BASE_URL setting is loaded as expected
+        """
+        self.assertSettingEnvExpected(
+            environment_name='DJANGO__CDMS_BASE_URL',
+            django_name='CDMS_BASE_URL',
+            default='https://example.com',
+        )
+
+
+class TestAssertSettingsEnvExpected(TestCase, EnvSettingsAssertion):
+    """
+    Given an environment variable '__ENV_NAME__'
+    Given a Django setting '__DJANGO_NAME__'
+    Given a default value for the Django setting of '__DEFAULT__'
+
+    Then test the following states:
+
+    ----------------+-------------------+-----------
+     __ENV_NAME__   | __DJANGO_NAME__   | Result
+    ----------------+-------------------+-----------
+     None           | '__DEFAULT__'     | Pass
+     None           | '__OTHER__'       | Fail
+     '__VALUE__'    | '__DEFAULT__'     | Fail
+     '__VALUE__'    | '__VALUE__'       | Pass
+     '__VALUE__'    | '__OTHER__'       | Fail
+    ----------------+-------------------+-----------
+    """
+
+    def tearDown(self):
+        """
+        Remove any left over envvar otherwise it'll hang over to the next test
+        """
+        try:
+            del(os.environ['__ENV_NAME__'])
+        except KeyError:
+            pass
+        super().tearDown()
+
+    def test_set_up(self):
+        """
+        setUp: assertSettingEnvExpected tests start with no env var
+        """
+        self.assertFalse('__ENV_NAME__' in os.environ)
+
+    @override_settings(__DJANGO_NAME__='__DEFAULT__')
+    def test_none_default(self):
+        """
+        assertSettingEnvExpected: No envvar, Django setting is default value
+        """
+
+        self.assertSettingEnvExpected('__ENV_NAME__', '__DJANGO_NAME__', '__DEFAULT__')
+
+    @override_settings(__DJANGO_NAME__='__OTHER__')
+    def test_none_other(self):
+        """
+        assertSettingEnvExpected: No envvar, Django setting other value
+        """
+        try:
+            del(os.environ['__ENV_NAME__'])
+        except KeyError:
+            pass
+
+        with self.assertRaises(AssertionError):
+            self.assertSettingEnvExpected('__ENV_NAME__', '__DJANGO_NAME__', '__DEFAULT__')
+
+    @override_settings(__DJANGO_NAME__='__DEFAULT__')
+    def test_value_default(self):
+        """
+        assertSettingEnvExpected: envvar has value, setting is default
+        """
+        os.environ['__ENV_NAME__'] = '__VALUE__'
+
+        with self.assertRaises(AssertionError):
+            self.assertSettingEnvExpected('__ENV_NAME__', '__DJANGO_NAME__', '__DEFAULT__')
+
+    @override_settings(__DJANGO_NAME__='__VALUE__')
+    def test_value_value(self):
+        """
+        assertSettingEnvExpected: envvar has value, setting is value
+        """
+        os.environ['__ENV_NAME__'] = '__VALUE__'
+
+        self.assertSettingEnvExpected('__ENV_NAME__', '__DJANGO_NAME__', '__DEFAULT__')
+
+    @override_settings(__DJANGO_NAME__='__OTHER__')
+    def test_value_other(self):
+        """
+        assertSettingEnvExpected: envvar has value, setting is other
+        """
+        os.environ['__ENV_NAME__'] = '__VALUE__'
+
+        with self.assertRaises(AssertionError):
+            self.assertSettingEnvExpected('__ENV_NAME__', '__DJANGO_NAME__', '__OTHER__')
