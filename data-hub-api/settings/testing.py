2dbda5ad6c508925c007b48eb013d8836e31d40d
Bowen Sun <bowensun@gmail.com>
2016-08-25 11:48:23 +0100

    Jenkins job

--- data-hub-api/settings/testing.py
+++ data-hub-api/settings/testing.py
@@ -24,5 +24,16 @@ CDMS_USERNAME = os.environ.get('DJANGO__CDMS_USERNAME', 'username')
 CDMS_PASSWORD = os.environ.get('DJANGO__CDMS_PASSWORD', 'password')
 CDMS_COOKIE_KEY = b'RKfgWE-GYNy3mWHm5wEUZavralDzSMKDguBfyuBag8A='
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DJANGO__DB_NAME', 'data-hub-api'),
+        'USER': os.environ.get('DJANGO__DB_USERNAME', ''),
+        'PASSWORD': os.environ.get('DJANGO__DB_PASSWORD', ''),
+        'HOST': os.environ.get('DJANGO__DB_HOST', ''),
+        'PORT': os.environ.get('DJANGO__DB_PORT', '5432'),
+    }
+}
+
 if os.environ.get('DJANGO__TEST_INTEGRATION'):
     TEST_INTEGRATION = True
