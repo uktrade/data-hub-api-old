7b7db209c7eec33cd706b77391483db6aa82e5ae
James Cooke <jamescooke@users.noreply.github.com>
2016-08-25 14:06:41 +0100

    Extend tests on update

--- data-hub-api/apps/cdms_api/tests/integration/accounts/test_update.py
+++ data-hub-api/apps/cdms_api/tests/integration/accounts/test_update.py
@@ -1,5 +1,4 @@
-import unittest
-
+from ....exceptions import ErrorResponseException
 from ..client_test_case import ClientTestCase
 
 
@@ -53,7 +52,7 @@ class TestUpdate(ClientTestCase):
         Client can update an existing record with partial data
 
         Erné has become a title member, but the phone number has stayed the
-        same.
+        same. Other records remain the same.
         """
         data = {
             'Name': 'Børt and Erné PLC.',
@@ -63,17 +62,36 @@ class TestUpdate(ClientTestCase):
 
         self.assertEqual(result['Name'], 'Børt and Erné PLC.')
         self.assertEqual(result['Telephone1'], '+32 11 22 33')
+        a_account = self.client.get('Account', self.guids['a'])
+        self.assertEqual(a_account['Name'], 'Á la café Ltd')
+        self.assertEqual(a_account['Telephone1'], '112233')
+        c_account = self.client.get('Account', self.guids['c'])
+        self.assertEqual(c_account['Name'], 'Crapola Ltd.')
+        self.assertEqual(c_account['Telephone1'], '__number__')
 
-    @unittest.skip('')
     def test_happy_add(self):
         """
-        Client can update an existing record with more data
+        Client can update an existing record with extra data
         """
+        data = {
+            'Telephone2': '+1 234 567 890 ext 1234',
+        }
 
-    @unittest.skip('')
-    def test_extra_data(self):
-        pass
+        result = self.client.update('Account', self.guids['b'], data)
 
-    @unittest.skip('')
+        self.assertEqual(result['Name'], 'Børt Limited')
+        self.assertEqual(result['Telephone1'], '+32 11 22 33')
+        self.assertEqual(result['Telephone2'], '+1 234 567 890 ext 1234')
+
     def test_missing(self):
-        pass
+        """
+        Client attempts to add field that does not exist, raises
+        """
+        data = {
+            '__OTHER__': 1,
+        }
+
+        with self.assertRaises(ErrorResponseException) as context:
+            self.client.update('Account', self.guids['b'], data)
+
+        self.assertEqual(context.exception.status_code, 400)
